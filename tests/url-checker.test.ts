3e787a51bf1cf60fe613e25f67fb47712fc2b2fe
jaana <jaana.jegorova@gmail.com>
2025-01-10 22:30:51 +0200

    fix/jj/lesson8_url-checker

--- tests/url-checker.test.ts
+++ tests/url-checker.test.ts
@@ -1,35 +1,31 @@
-import {LinkChecker} from '../src/url-checker'
+import { LinkChecker } from "../src/url-checker";
 
+describe("LinkChecker", () => {
+  let linkChecker: LinkChecker;
 
-describe('LinkChecker', () => {
-    let linkChecker: LinkChecker;
+  beforeEach(() => {
+    linkChecker = new LinkChecker();
+  });
 
-    beforeEach(() => {
-        linkChecker = new LinkChecker();
-    });
+  test("should return true for valid URL with https", () => {
+    expect(linkChecker.isValidUrl("https://www.example.com")).toBe(true);
+  });
 
-    test('should return true for valid URL with https', () => {
-        expect(linkChecker.isValidUrl('https://www.example.com')).toBe(true);
-    })
+  test("should return true for valid URL with http", () => {
+    expect(linkChecker.isValidUrl("http://www.example.com")).toBe(true);
+  });
 
-    // FIXME
-    test('should return true for valid URL with http', () => {
-        expect(linkChecker.isValidUrl('http://www.example.com'))
-    })
+  test("should return false for invalid URL with no protocol", () => {
+    expect(linkChecker.isValidUrl("www.example.com")).toBe(false);
+  });
 
-    // FIXME
-    test('should return false for invalid URL with no protocol', () => {
-        expect(linkChecker.isValidUrl('www.example.com')).toBeNaN()
-    })
+  test("should return true for valid URL with port", () => {
+    expect(linkChecker.isValidUrl("http://www.example.com:8080")).toBe(true);
+  });
 
-    // FIXME:
-
-    // test('should return true for valid URL with port', () => {
-    //     linkChecker.isValidUrl('http://www.example.com:8080')).toBetrue;
-    // })
-    //
-    // test('should return true for valid URL with path', () => {
-    //     expect(linkCheckker.isValidUrl('http://www.example.com/path/to/page')).toBeFalsy()
-    // })
-
-})
+  test("should return true for valid URL with path", () => {
+    expect(
+      linkChecker.isValidUrl("http://www.example.com/path/to/page"),
+    ).toBeTruthy();
+  });
+});
